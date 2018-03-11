8fa347f84045cbc6cc51fb877e1c03f42aa126b1
SmallTiger2 <2425513303@qq.com>
2018-03-12 01:08:47 +0800

    双星云犬——正式开发版

---
+++ app/src/main/java/helloworld/dongnaoedu/com/ds_login/MainActivity.java
@@ -0,0 +1,28 @@
+package helloworld.dongnaoedu.com.ds_login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ds_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
